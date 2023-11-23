2945674fece4f8a9442b77358f0aa75de2106103
SvdNeo <sanjay.d@neoito.com>
2023-11-23 17:37:55 +0530

    completed the webpage design with revision 1

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const accordionItems = document.querySelectorAll('.accordion-item');
 
-    accordionItems.forEach(item => {
-      const input = item.querySelector('input');
-      const content = item.querySelector('.accordion-content');
-      const arrowIcon = item.querySelector('svg');
+accordionItems.forEach(item => {
+  const input = item.querySelector('input');
+  const content = item.querySelector('.accordion-content');
+  const arrowIcon = item.querySelector('svg');
 
-      input.addEventListener('change', () => {
-        if (input.checked) {
-          content.style.display = 'block';
-          arrowIcon.style.transform = 'rotate(180deg)';
-        } else {
-          content.style.display = 'none';
-          arrowIcon.style.transform = 'rotate(0deg)';
-        }
-      });
-    });
+  // Set the initial state to hide the content
+  content.style.display = 'none';
+
+  input.addEventListener('change', () => {
+    if (input.checked) {
+      content.style.display = 'block';
+      arrowIcon.style.transform = 'rotate(180deg)';
+    } else {
+      content.style.display = 'none';
+      arrowIcon.style.transform = 'rotate(0deg)';
+    }
+  });
+});
